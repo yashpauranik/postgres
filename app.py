15495b2938b6d637ac8cc545913d79e6f43d45bb
yashpauranik <107103139+yashpauranik@users.noreply.github.com>
2022-10-31 01:29:29 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import json
-import Infodao
-
-
-def lambda_handler(event, context):
-    infObj = event["first_name"] , event["last_name"] , event["email"] , event["is_email_verified"] , event["mobile"]
-    Infodao.insert(infObj)
-    return table
-lambda_handler(events,context)
